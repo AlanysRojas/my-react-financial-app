b494f2c42bbd5a2c48b07523e28a9de066d9848a
Alanys Rojas <alyfr2000@gmail.com>
2024-09-01 15:20:37 -0500

    [F1] List of financial products
    Se requiere una aplicación para visualizar los diferentes productos financieros ofertados por un
    Banco cargados de una API. Realizar la maquetación en base al diseño D1.

---
+++ src/services/productService.tsx
@@ -0,0 +1,29 @@
+import { Product } from "../types/Product";
+
+const API_URL =
+    process.env.REACT_APP_API_URL ||
+    "https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros/bp/products";
+
+const DEFAULT_HEADERS = {
+  authorId: process.env.REACT_APP_AUTHOR_ID || "123456",
+};
+
+export const getProducts = async (): Promise<Product[]> => {
+  try {
+    const response = await fetch(API_URL, {
+      headers: DEFAULT_HEADERS,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch products: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`An error occurred: ${error.message}`);
+    } else {
+      throw new Error("An unknown error occurred");
+    }
+  }
+};
